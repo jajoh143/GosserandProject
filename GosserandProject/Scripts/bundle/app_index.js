e0d4ff97935ff8a13bc4846463f11ec093570664
Jacob Johnson <jajoh143@gmail.com>
2018-04-04 20:51:55 -0500

    added in script bundles

---
+++ GosserandProject/Scripts/bundle/app_index.js
@@ -0,0 +1,25 @@
+webpackJsonp([0],{
+
+/***/ 127:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _vue = __webpack_require__(92);
+
+var _vue2 = _interopRequireDefault(_vue);
+
+var _vuetify = __webpack_require__(130);
+
+var _vuetify2 = _interopRequireDefault(_vuetify);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_vue2.default.use(_vuetify2.default);
+
+var app = new _vue2.default({}).$mount("#app");
+
+/***/ })
+
+},[127]);
