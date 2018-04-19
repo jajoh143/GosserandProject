a11fb6062633dd670c592d410505d7ef09f6732c
katrinawalker06 <katrinawalker06@gmail.com>
2018-04-18 19:46:58 -0500

    added the usermanagement controller

---
+++ GosserandProject/Controllers/UserManagementController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GosserandProject.Model.UserManagementViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GosserandProject.Controllers
+{
+	[Route("[controller]/[action]")]
+	public class UserManagementController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+			var model = new UserManagementViewModel;
+            return View(model);
+        }
+    }
+}
---
+++ GosserandProject/ViewModels/UserManaement/UserManagementViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace GosserandProject.Model.UserManagementViewModels
+{
+	public class UserManagementViewModel
+	{
+		public string FirstName { get; set;}
+		public string LastName	{ get; set;}
+		public string Username { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Postion { get; set; }
+		public string Email { get; set; }
+
+	}
+}
