ac5097e101036e7b0a249de114a0152997ee6ef1
katrinawalker06 <katrinawalker06@gmail.com>
2018-05-06 08:25:28 -0500

    fixing

--- GosserandProject/Controllers/UserManagementController.cs
+++ GosserandProject/Controllers/UserManagementController.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using GosserandProject.Model.UserManagementViewModels;
+using GosserandProject.Data.Commands;
+using GosserandProject.Data.DTO.Admin;
+
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace GosserandProject.Controllers
+namespace GosserandProject.Web.Controllers
 {
 	[Route("[controller]/[action]")]
 	public class UserManagementController : Controller
-    {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-			var model = new UserManagementViewModel;
-            return View(model);
-        }
-    }
+	{
+
+	}
 }
+ 
