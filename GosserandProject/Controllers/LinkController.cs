4031e3ffafd20241f41b7c8a3a61dcfff797ef3b
Jacob Johnson <jajoh143@gmail.com>
2018-05-20 10:31:00 -0500

    added in link  controller

---
+++ GosserandProject/Controllers/LinkController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GosserandProject.Models.ViewModels.FrontEnd;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace GosserandProject.Controllers
+{
+    public class LinkController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public LinkController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Link/{routeName:string}")]
+        public IActionResult RouteStaticPage(string routeName)
+        {
+            var model = new StaticPageViewModel(this._configuration.GetConnectionString("DefaultConnection"),routeName);
+            return View(model);
+        }
+    }
+}
---
+++ GosserandProject/Models/ViewModels/FrontEnd/StaticPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GosserandProject.Data.DTO.FrontEnd;
+using GosserandProject.ViewModels.FrontEnd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GosserandProject.Models.ViewModels.FrontEnd
+{
+    public class StaticPageViewModel : BaseFrontEndViewModel
+    {
+        public PageDisplayDTO PageDisplay { get; set; }
+
+        public StaticPageViewModel(string connString, string routeName) : base(connString)
+        {
+            PageDisplay = GetFrontEndData.GetStaticPage(routeName);
+        }
+    }
+}
